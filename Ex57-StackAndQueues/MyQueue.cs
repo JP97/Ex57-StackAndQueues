7ddbfba51211292b828eaa02f01fa102f4d97088
Julian <juli11c0@edu.eal.dk>
2019-03-06 08:50:33 +0100

    Add project files.

---
+++ Ex57-StackAndQueues/MyQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex57_StackAndQueues
+{
+	public class MyQueue<T>
+	{
+		// Insert local datastructures here ...
+
+		public MyQueue()
+		{
+		}
+
+		public void Enqueue(T item)
+		{
+		}
+
+		public T Dequeue()
+		{
+		}
+
+		public int Count()
+		{
+		}
+
+		public Boolean IsEmpty()
+		{
+		}
+	}
+}
+
+
---
+++ Ex57-StackAndQueues/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex57_StackAndQueues
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
